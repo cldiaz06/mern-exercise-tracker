e66125acad73c0296403722f3daf920de04226ee
cldiaz06 <cldiaz06@gmail.com>
2019-12-31 09:22:28 -0600

    added routes and api endpoints

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,9 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
+const exercisesRouter = require('./routes/exercises');
+const usersRouter = require('./routes/users');
+
 mongoose.connect('mongodb://localhost/')
     .then(() => {
         console.log('Connected to DB');
@@ -18,6 +21,9 @@ mongoose.connect('mongodb://localhost/')
 app.use(cors());
 app.use(express.json());
 
+app.use('/exercises', exercisesRouter);
+app.use('/users', usersRouter);
+
 app.listen(port, () => {
-    console.log("server running at port: ${port}");
+    console.log('server running at port: ' + port);
 });
